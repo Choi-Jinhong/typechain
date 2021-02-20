83cb64aad435ff8041a8b77880eb54d829111ac7
Gnong <jinhongdev@gmail.com>
2021-02-21 00:25:21 +0900

    How to static Method & set blockchain function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import * as CyptoJS from 'crypto-js';
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    // static을 사용하지 않으면 Block을 선언한 후에 다음 메소드를 사욯할 수 있지만, 
+    // static을 사용하게 되면 선언하지 않고도 사용이 가능하다.
+    static calculateBlockHash = (index: number, preiousHash: string, timestamp: number, data: string)
+    : string => CyptoJS.SHA256(index + preiousHash + timestamp + data).toString();
+
     constructor(index: number,
         hash: string,
         previousHash: string,
@@ -20,7 +28,12 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "221009f0shfoiadf", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
 export { };
