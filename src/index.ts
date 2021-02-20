38a29f604d96ed45fe2ca265a18208ba510fe0f9
Gnong <jinhongdev@gmail.com>
2021-02-21 00:05:16 +0900

    How to use class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human { // interface, class는 비슷한 기능을 하지만 상황에 따라서 사용하게 됨 (class: react와 같은 클래스가 필요할 때 사용, interface 대부분 사용)
+    public name: string; //public , private이 있음
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender?:string) { // ?도 사용가능
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "gnong",
-    age: 27,
-    gender: "male"
-}
+const lynn = new Human("Lynn", 18, "famale");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}. you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
